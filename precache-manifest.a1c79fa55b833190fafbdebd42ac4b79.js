5f49f7e56fcd943c7e42d7ad4b3de38442e425d5
Jacob Haugen <howagain@gmail.com>
2020-08-24 17:28:04 -0400

    Updates

---
+++ precache-manifest.a1c79fa55b833190fafbdebd42ac4b79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b69c16048f499e36c09b705133306a",
+    "url": "/fcc-pomodoro-timer/index.html"
+  },
+  {
+    "revision": "0e3bb3b3f37df3629297",
+    "url": "/fcc-pomodoro-timer/static/css/main.7ae7c8e1.chunk.css"
+  },
+  {
+    "revision": "3ea187411fe6812db506",
+    "url": "/fcc-pomodoro-timer/static/js/2.d24de510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-pomodoro-timer/static/js/2.d24de510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3bb3b3f37df3629297",
+    "url": "/fcc-pomodoro-timer/static/js/main.b1fd3d9c.chunk.js"
+  },
+  {
+    "revision": "66a646a5780f61664bd6",
+    "url": "/fcc-pomodoro-timer/static/js/runtime-main.64a75a16.js"
+  },
+  {
+    "revision": "09bb07c014654fba0e0c2e9852eb09ba",
+    "url": "/fcc-pomodoro-timer/static/media/Elevator-ding-sound-effect.09bb07c0.mp3"
+  }
+]);
